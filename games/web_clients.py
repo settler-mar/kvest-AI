9e507634cc329334da862a4cc9869b004e4685cf
Maxim Matuhin <matuhinmax@gmail.com>
2023-07-01 15:25:13 +0300

    Create GAMES display class

--- games/web_clients.py
+++ games/web_clients.py
@@ -10,28 +10,9 @@ pages = [
     ('http://127.0.0.1:8080/snake.html', 3)
 ]
 
-mouse_display = -1  # номер экрана на котором мыш. усли -1 то эмитирует клавиатуру
-
-# Получение списка дисплеев
-monitors = get_monitors()
-
-
-def print_monitor_info():
-    # Перебор дисплеев и получение их свойств
-    for i, monitor in enumerate(monitors, 1):
-        print("Дисплей №", i)
-        print("Разрешение:", monitor.width, "x", monitor.height)
-        print("Смещение по X:", monitor.x)
-        print("Смещение по Y:", monitor.y)
-        print()
-
-
-print_monitor_info()
-
 
 class Display:
-    def __init__(self, page, display_number):
-        monitor = monitors[display_number if display_number < len(monitors) else 0]
+    def __init__(self, page, monitor):
         options = Options()
         options.add_experimental_option("excludeSwitches", ['enable-automation'])
         self.driver = webdriver.Chrome(options=options)
@@ -48,74 +29,94 @@ class Display:
         self.driver.fullscreen_window()
 
 
-displays = [Display(url, display_number) for url, display_number in pages]
+class MouseControl:
+    mouse_display = -1  # номер экрана на котором мыш. усли -1 то эмитирует клавиатуру
+    monitors = get_monitors()
 
-x_min, y_min = 100, 100
-x_max, y_max = 500, 500
+    x_min, y_min = 100, 100
+    x_max, y_max = 500, 500
 
+    def __init__(self):
+        self.print_monitor_info()
+        self.displays = [Display(url,
+                                 self.monitors[display_number if display_number < len(self.monitors) else 0])
+                         for url, display_number in pages]
+        self.set_pos()
 
-def set_pos():
-    global x_min, y_min, x_max, y_max
-    if mouse_display == -1:
-        x_min = monitors[0].x + monitors[0].width / 2
-        y_min = monitors[0].y + monitors[0].height / 2
-        pyautogui.moveTo(x_min, y_min, duration=0.1)
-        return
-    x_min = monitors[0].x
-    y_min = monitors[0].y
-    x_max = monitors[0].x + monitors[0].width
-    y_max = monitors[0].y + monitors[0].height
+    def print_monitor_info(self):
+        # Перебор дисплеев и получение их свойств
+        for i, monitor in enumerate(self.monitors, 1):
+            print("Дисплей №", i)
+            print("Разрешение:", monitor.width, "x", monitor.height)
+            print("Смещение по X:", monitor.x)
+            print("Смещение по Y:", monitor.y)
+            print()
 
-    pyautogui.moveTo((x_min + x_max) / 2, (y_min + y_max) / 2, duration=0.1)
+    def set_pos(self):
+        if self.mouse_display == -1:
+            x_min = self.monitors[0].x + self.monitors[0].width / 2
+            y_min = self.monitors[0].y + self.monitors[0].height / 2
+            pyautogui.moveTo(x_min, y_min, duration=0.1)
+            return
+        self.x_min = self.monitors[0].x
+        self.y_min = self.monitors[0].y
+        self.x_max = self.monitors[0].x + self.monitors[0].width
+        self.y_max = self.monitors[0].y + self.monitors[0].height
 
+        pyautogui.moveTo((self.x_min + self.x_max) / 2, (self.y_min + self.y_max) / 2, duration=0.1)
 
-set_pos()
+    def update_mouse_pos(self):
+        x, y = pyautogui.position()
 
-
-def on_key_press(key):
-    if key == keyboard.Key.esc:
-        print("Клавиша Esc нажата. Программа завершена.")
-        return False  # Останавливаем прослушивание клавиш
-
-
-listener = keyboard.Listener(on_press=on_key_press)
-listener.start()
-
-while listener.is_alive():
-    x, y = pyautogui.position()
-
-    if mouse_display == -1:
-        dx = x - x_min
-        dy = y - y_min
-        if abs(dx) > 2 or abs(dy) > 2:
-            if abs(dx) > abs(dy):
-                if dx > 0:
-                    # pyautogui.press("left")
-                    pyautogui.keyDown("right")
-                    pyautogui.keyUp("right")
+        if self.mouse_display == -1:
+            dx = x - self.x_min
+            dy = y - self.y_min
+            if abs(dx) > 2 or abs(dy) > 2:
+                if abs(dx) > abs(dy):
+                    if dx > 0:
+                        pyautogui.keyDown("right")
+                        pyautogui.keyUp("right")
+                    else:
+                        pyautogui.keyDown('left')
+                        pyautogui.keyUp('left')
                 else:
-                    pyautogui.keyDown('left')
-                    pyautogui.keyUp('left')
-            else:
-                if dy > 0:
-                    pyautogui.keyDown('down')
-                    pyautogui.keyUp('down')
-                else:
-                    pyautogui.keyDown('up')
-                    pyautogui.keyUp('up')
-            print(dx, dy)
-        pyautogui.moveTo(x_min, y_min, duration=0.1)
-        continue
+                    if dy > 0:
+                        pyautogui.keyDown('down')
+                        pyautogui.keyUp('down')
+                    else:
+                        pyautogui.keyDown('up')
+                        pyautogui.keyUp('up')
+                print(dx, dy)
+            pyautogui.moveTo(self.x_min, self.y_min, duration=0.1)
+            return
 
-    # Проверяем и ограничиваем координаты курсора
-    if x < x_min:
-        x = x_min
-    elif x > x_max:
-        x = x_max
-    if y < y_min:
-        y = y_min
-    elif y > y_max:
-        y = y_max
+        # Проверяем и ограничиваем координаты курсора
+        if x < self.x_min:
+            x = self.x_min
+        elif x > self.x_max:
+            x = self.x_max
+        if y < self.y_min:
+            y = self.y_min
+        elif y > self.y_max:
+            y = self.y_max
 
-    # Перемещаем курсор в ограниченные координаты
-    pyautogui.moveTo(x, y, duration=0.1)
+        # Перемещаем курсор в ограниченные координаты
+        pyautogui.moveTo(x, y, duration=0.1)
+
+    def processed(self):
+        self.update_mouse_pos()
+
+
+def main():
+    def on_key_press(key):
+        if key == keyboard.Key.esc:
+            print("Клавиша Esc нажата. Программа завершена.")
+            return False  # Останавливаем прослушивание клавиш
+
+    listener = keyboard.Listener(on_press=on_key_press)
+    listener.start()
+
+    mouse_control = MouseControl()
+
+    while listener.is_alive():
+        mouse_control.processed()
